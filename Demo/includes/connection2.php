67e7f12b67b1ba849bc0e925d86d54ecda687a43
ntran97 <n.tran.1@hotmail.com>
2018-10-23 15:03:42 +1000

    asdf

--- Demo/includes/connection2.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $username = "root";
-    $password = "";
-    $hostname = "localhost"; 
-    $dbname = "unlocked";
-    $db_conn = mysqli_connect($hostname, $username, $password,$dbname) or die("Could not connect to db");   
-?>
